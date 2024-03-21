5ca9b93f2dfa5debb2ba73f1cf89e4d983c823a1
Amber Ozenogu <amberozenogu@gmail.com>
2024-03-21 22:20:12 +0100

    JS task 4

---
+++ Module3/3A(JavaScript)/task4.js
@@ -0,0 +1,9 @@
+//step 1
+const day = "Thursday";
+//step 2
+const position = 4;
+//step 3
+const output = "Today is " + day + ". It is the " + position + "th day of the week.";
+
+//print output
+console.log(output);
