b927840c5d1fe9a69d3b3c63a5e7aa64e5c5f7d6
Amber Ozenogu <amberozenogu@gmail.com>
2024-03-27 19:37:44 +0100

    JS task 2

---
+++ Module3/3A(JavaScript)/task12.js
@@ -0,0 +1,7 @@
+
+const friends = ['Becca', 'Rose', 'Winnie', 'Maureen', 'Cassie']
+
+console.log(friends);
+
+const bestFriend = friends[0]
+console.log(bestFriend);
--- Module3/3A(JavaScript)/task2.js
+++ Module3/3A(JavaScript)/task2.js
@@ -1,7 +1,7 @@
 console.log("JavaScript Fun Facts");
 
 /*
-  JS Fun Facts!
+  JS Fun Facts!!
   1. JavaScript took just 10 days to develop.
   2. JavaScript was probably named after Java.
   3. GitHub says JavaScript is the Most Popular language in the world.
