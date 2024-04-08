8f23957d8ba365f6926a3a53d806f60c544fd721
Amber Ozenogu <amberozenogu@gmail.com>
2024-04-08 19:02:44 +0100

    JS challenge 6

---
+++ Module3/3A(JavaScript)/JSCodingChallenge/challenge6.js
@@ -0,0 +1,6 @@
+// Sort an array of strings in alphabetical order.
+
+const fruits = ["Pear", "Banana", "Mango", "Apple", "Strawberry", "Orange", "Pineapple", "Grape", "Cranberry"];
+fruits.sort();
+
+console.log(fruits);
