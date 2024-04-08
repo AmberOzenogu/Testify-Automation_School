6292e577d957677e5565e198cab6ed0896efd1c5
Amber Ozenogu <amberozenogu@gmail.com>
2024-04-08 19:07:02 +0100

    JS challenge 7

---
+++ Module3/3A(JavaScript)/JSCodingChallenge/challenge7.js
@@ -0,0 +1,6 @@
+// Sort an array of numbers in descending order.
+
+const numbersArray = [51, 32, 5, 81, 90, 11, 2, 17, 68, 1, 99, 8];
+numbersArray.sort((a, b) => b - a);
+
+console.log(numbersArray);
