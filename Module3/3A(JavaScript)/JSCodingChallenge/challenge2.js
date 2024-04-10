0932c24ce0449df3115edcd3dc58b0b8b6f2c0a5
Amber Ozenogu <amberozenogu@gmail.com>
2024-04-10 14:39:24 +0100

    JS challenge 2

--- Module3/3A(JavaScript)/JSCodingChallenge/challenge2.js
+++ Module3/3A(JavaScript)/JSCodingChallenge/challenge2.js
@@ -1,1 +1,12 @@
 // Create a length converter function.
+
+function lengthConverter(meter){
+    // Convert meters to centimeters (1 meter = 100 centimeters)
+    const centimeter = meter * 100;
+    return centimeter;
+}
+
+const length = 41;
+const convertedLength = lengthConverter(length);
+
+console.log(length + " meters is equivalent to " + convertedLength + " centimeters");
