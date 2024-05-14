e01f2ace8a4c562acda0995d63eca8fbfe9941ee
Amber Ozenogu <amberozenogu@gmail.com>
2024-05-14 15:11:51 +0100

    Java Task 4

---
+++ Module4/Tasks_4A/src/Task4.java
@@ -0,0 +1,11 @@
+public class Task4 {
+    public static void main(String[] args) {
+        // Area of a circle = pi * radius * radius
+        double area = 3.14; //The value of pi
+        double radius = 12.7;
+
+        //Calculate the area of a circle using compound operator
+        area *= radius * radius;
+        System.out.println("The area of the circle is " + area + "meters.");
+    }
+}
