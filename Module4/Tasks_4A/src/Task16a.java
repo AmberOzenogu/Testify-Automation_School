8edc0a25ef1b2f87ee6c792f473b74163872efa7
Amber Ozenogu <amberozenogu@gmail.com>
2024-05-22 12:31:12 +0100

    Java Task 16

--- Module4/Tasks_4A/src/Task16a.java
+++ Module4/Tasks_4A/src/Task16a.java
@@ -4,14 +4,14 @@ public class Task16a {
     }
     // 1. difference in number of parameters of the same data type
     public void myMethod(String name){
-
+        System.out.println("Testify academy"); //Task16B
     }
     public void myMethod(String lastName, String text){
 
     }
     // 2. difference in data types
     public void myMethod(int number, String word){
-
+        System.out.println("The number and word chosen"); //Task16B
     }
     // 3. difference in sequence or position of data types
     public void myMethod(String newWord, int newNumber){
