bed644d122a9b234af66920ce4dd8bce53dcf431
Amber Ozenogu <amberozenogu@gmail.com>
2024-05-22 12:11:54 +0100

    Java Task 15

---
+++ Module4/Tasks_4A/src/Task16a.java
@@ -0,0 +1,20 @@
+public class Task16a {
+    public void myMethod(){
+
+    }
+    // 1. difference in number of parameters of the same data type
+    public void myMethod(String name){
+
+    }
+    public void myMethod(String lastName, String text){
+
+    }
+    // 2. difference in data types
+    public void myMethod(int number, String word){
+
+    }
+    // 3. difference in sequence or position of data types
+    public void myMethod(String newWord, int newNumber){
+
+    }
+}
