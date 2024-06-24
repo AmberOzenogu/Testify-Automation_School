e1661d7ff9acf94b1d258b8031e3f0d3a03fb5de
Amber Ozenogu <amberozenogu@gmail.com>
2024-06-24 13:50:58 +0100

    4B Tasks

---
+++ Module4/Tasks_4B/src/test/java/Task17c.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Task17c {
+    WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\edeno\\Desktop\\Testify\\Testify-Automation_School\\Module4\\Tasks_4B\\src\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void widgetsPage() throws InterruptedException {
+        driver.get("https://demoqa.com/");
+        Thread.sleep(1000);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 200)");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/*[1]")).click();
+        Thread.sleep(2000);
+
+        SoftAssert softA = new SoftAssert();
+        softA.assertTrue(driver.getCurrentUrl().contains("widgets"));
+        softA.assertAll();
+    }
+
+    @AfterTest
+    public void quitBrowser() {
+        driver.quit();
+    }
+}
