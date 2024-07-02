7d6d80b6e5392abed0a85e1883bb046439c5b788
Amber Ozenogu <amberozenogu@gmail.com>
2024-07-02 19:48:29 +0100

    4C Tasks

---
+++ Module4/Tasks_4C/task4.py
@@ -0,0 +1,9 @@
+# multiply
+numbers = 8 * 6
+print(numbers)
+
+# add
+num1 = 8
+num2 = 6
+total = num1 + num2
+print(total)
---
+++ Module4/Tasks_4C/task6.py
@@ -0,0 +1,8 @@
+num1 = 51
+num2 = 80
+if num1 > 50 and num2 < 100:
+    print("Evaluation is True")
+
+text = "python"
+if text == "testify" or text == "python":
+    print("Yay!! The string is a Testify")
