a56c90186402ac8698f893bac3f82bd7e6b4df87
Amber Ozenogu <amberozenogu@gmail.com>
2024-07-02 14:51:48 +0100

    4C Tasks

---
+++ Module4/Tasks_4C/task2.py
@@ -0,0 +1,11 @@
+# floating-point
+my_var = 12.6
+print(type(my_var))
+
+# integer
+my_var = 200
+print(type(my_var))
+
+# string
+my_var = "Amber"
+print(type(my_var))
---
+++ Module4/Tasks_4C/task3.py
@@ -0,0 +1,9 @@
+# Declare a raw string
+text = r"This is a \nNew line."
+print(text)
+
+color = "blue"
+item = "tooth"
+# Concatenate the two string
+my_var = color + item
+print(my_var)
